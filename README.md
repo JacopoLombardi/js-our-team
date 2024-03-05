70c7cc6c4dd46490ccf627f64e5d1a4ed88c4956
JacopoLombardi <lombajacopo@gmail.com>
2024-03-05 14:55:33 +0100

    [add] Setup con Array degli oggetti

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# js-our-team
+Our Team
+===
+1. Creo su Js l'array degli oggetti.
+2. Con un ciclo stampo in console le proprietà degli oggetti.
+3. 
+4. 
+5. 
---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+const team = [
+   {
+      nome: 'Wayne Barnett',
+      ruolo: 'Founder & CEO',
+      immagine: ''
+   },
+   {
+      nome: 'Angela Caroll',
+      ruolo: 'Chief Editor',
+      immagine: ''
+   },
+   {
+      nome: 'Walter Gordon',
+      ruolo: 'Office Manager',
+      immagine: ''
+   },
+   {
+      nome: 'Angela Lopez',
+      ruolo: 'Social Media Manager',
+      immagine: ''
+   },
+   {
+      nome: 'Scott Estrada',
+      ruolo: 'Developer',
+      immagine: ''
+   },
+   {
+      nome: 'Barbara Ramos',
+      ruolo: 'Graphic Designer',
+      immagine: ''
+   },
+];
+
+
+
+
+
+
+
