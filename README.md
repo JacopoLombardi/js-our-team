d00cc097539579a544a17832988eef6ef0089c16
JacopoLombardi <lombajacopo@gmail.com>
2024-03-05 15:36:29 +0100

    [add] task 2 - 3 completed

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Our Team
 ===
 1. Creo su Js l'array degli oggetti.
 2. Con un ciclo stampo in console le proprietà degli oggetti.
-3. 
+3. Strutturo uno scheletro dell'HTML per poi costruirlo in Js.
 4. 
 5. 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 
+const output = document.querySelector('._output');
+
+// array con oggetti all'interno
 const team = [
    {
       nome: 'Wayne Barnett',
@@ -34,7 +37,23 @@ const team = [
 
 
 
+for(people of team){
 
+   output.innerHTML += `
+   
+   <div class="col-3  card  text-center  rounded-0  mx-1  mt-5  p-0">
+      <img src="img/wayne-barnett-founder-ceo.jpg" alt="...">
+      <div class="card-body">
+         <h5 class="card-title">${people.nome}</h5>
+         <p class="card-text">${people.ruolo}</p>
+      </div>
+   </div>
+   
+   `;
+
+   console.log(people)
+
+}
 
 
 
